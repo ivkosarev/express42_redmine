9e42bcb5b62539af2e222e1ace73b1235fa551ea
Ivan Kosarev <ivkosarev@gmail.com>
2020-10-19 19:39:11 +0300

    try dockerfile redmine

--- start.sh
+++ start.sh
@@ -30,5 +30,5 @@ bundle install
 bundle exec rake generate_secret_token
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
-ruby bin/rails server -b 0.0.0.0 -e production
+#ruby bin/rails server -b 0.0.0.0 -e production
 
