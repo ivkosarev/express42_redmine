9f76b999db06b8818e8e3e8ac804272f407745c4
Ivan Kosarev <ivkosarev@gmail.com>
2020-10-19 19:03:44 +0300

    try dockerfile redmine

--- start.sh
+++ start.sh
@@ -10,9 +10,8 @@ svn co https://svn.redmine.org/redmine/branches/4.1-stable redmine-4.1
 ln -s redmine-* redmine
 pg_ctlcluster 12 main start
 groupadd postgresusers && usermod -aG postgresusers,sudo postgres && chgrp postgresusers /opt/
-pg_ctl reload
-sudo -u postgres psql -h localhost -c "CREATE ROLE redmine LOGIN ENCRYPTED PASSWORD 'super_strong_passwd_123' NOINHERIT VALID UNTIL 'infinity'"
-sudo -u postgres psql -h localhost -c "CREATE DATABASE redmine WITH ENCODING='UTF8' OWNER=redmine"
+sudo -u postgres psql -c "CREATE ROLE redmine LOGIN ENCRYPTED PASSWORD 'super_strong_passwd_123' NOINHERIT VALID UNTIL 'infinity'"
+sudo -u postgres psql -c "CREATE DATABASE redmine WITH ENCODING='UTF8' OWNER=redmine"
 cd /opt/redmine-4.1
 echo "production:" > config/database.yml
 echo " adapter: postgresql" >> config/database.yml
