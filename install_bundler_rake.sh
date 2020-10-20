471e7cb823194dff3a59a7e92a16ee83774d92a0
Ivan Kosarev <ivkosarev@gmail.com>
2020-10-20 16:55:18 +0300

    try dockerfile redmine

--- install_bundler_rake.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-gem install bundler 
-bundle config set without 'development test rmagick'
-cd /opt/redmine-4.1 && bundle install
-bundle exec rake generate_secret_token
-RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
