6af6af6278ec3946e2ac448f855ee2f66620c7cf
Ivan Kosarev <ivkosarev@gmail.com>
2020-10-20 16:11:46 +0300

    try dockerfile redmine

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ groupadd postgresusers && usermod -aG postgresusers,sudo postgres && chgrp postg
 sudo -u postgres psql -c "CREATE ROLE redmine LOGIN ENCRYPTED PASSWORD 'super_strong_passwd_123' NOINHERIT VALID UNTIL 'infinity'"
 sudo -u postgres psql -c "CREATE DATABASE redmine WITH ENCODING='UTF8' OWNER=redmine"
 cd /opt/redmine-4.1 && bundle install
+source /usr/local/rvm/scripts/rvm
+rvm use 2.6.5
 bundle exec rake generate_secret_token
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
