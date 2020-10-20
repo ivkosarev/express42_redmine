07dda20c6753820e892a10996cc9a671ffcd67c9
Ivan Kosarev <ivkosarev@gmail.com>
2020-10-20 15:27:50 +0300

    try dockerfile redmine

--- init.sh
+++ init.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 pg_ctlcluster 12 main start
-groupadd postgresusers && usermod -aG postgresusers,sudo postgres && chgrp postgresusers /opt/
-sudo -u postgres psql -c "CREATE ROLE redmine LOGIN ENCRYPTED PASSWORD 'super_strong_passwd_123' NOINHERIT VALID UNTIL 'infinity'"
-sudo -u postgres psql -c "CREATE DATABASE redmine WITH ENCODING='UTF8' OWNER=redmine"
 ruby bin/rails server -b 0.0.0.0 -e production
