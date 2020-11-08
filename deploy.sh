4009d526c21f52ebd2763286873d923dc2297611
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-08 19:08:12 +0300

    add traviv.yml

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+docker build -t  redmine_all_in_one ./redmine
+docker tag redmine_all_in_one ivkosarev/redmine_all_in_one:latest
+
+echo $my_passwd_docker | docker login --username ivkosarev --password-stdin
+
+docker push ivkosarev/redmine_all_in_one:latest
+
+if [ $? -eq 0 ]
+then
+  echo "Build Successfully Pushed!"
+else
+  echo "Something went wrong" >&2
+fi
