9b2dad4986a82eab45fbb419de9b26144c45c2c6
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-08 12:14:23 +0300

    fix

--- init.sh
+++ init.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 pg_ctlcluster 12 main start
-groupadd postgresusers && usermod -aG postgresusers,sudo postgres && chgrp postgresusers /opt/
 sudo -u postgres psql -c "CREATE ROLE redmine LOGIN ENCRYPTED PASSWORD 'super_strong_passwd_123' NOINHERIT VALID UNTIL 'infinity'"
 sudo -u postgres psql -c "CREATE DATABASE redmine WITH ENCODING='UTF8' OWNER=redmine"
-cd /opt/redmine-4.1 && bundle install
-source /usr/local/rvm/scripts/rvm
-rvm use system
 bundle exec rake generate_secret_token
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
--- ruby.sh
+++ ruby.sh
@@ -4,5 +4,5 @@ bundle config set without 'development test rmagick'
 cd /opt/redmine-4.1 && bundle install
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 source /usr/local/rvm/scripts/rvm
-rvm use system
+rvm use 2.6.5
 
