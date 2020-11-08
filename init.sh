dbb7ee4e73c2ed04048f46c09c0c0dc50b3d54bb
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-08 12:54:32 +0300

    +

--- init.sh
+++ init.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-pg_ctlcluster 12 main start
 ruby bin/rails server -b 0.0.0.0 -e production
--- ruby.sh
+++ ruby.sh
@@ -9,6 +9,7 @@ bundle config set without 'development test rmagick'
 cd /opt/redmine-4.1 && bundle install
 bundle exec rake generate_secret_token
 groupadd postgresusers && usermod -aG postgresusers,sudo postgres && chgrp postgresusers /opt/
+pg_ctlcluster 12 main start
 sudo -u postgres psql -c "CREATE ROLE redmine LOGIN ENCRYPTED PASSWORD 'super_strong_passwd_123' NOINHERIT VALID UNTIL 'infinity'"
 sudo -u postgres psql -c "CREATE DATABASE redmine WITH ENCODING='UTF8' OWNER=redmine"
 RAILS_ENV=production bundle exec rake db:migrate
