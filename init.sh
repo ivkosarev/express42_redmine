18d26a3166417c82c8061e4bf93362835405d46b
Ivan Kosarev <ivkosarev@gmail.com>
2020-11-08 12:49:15 +0300

    +

--- init.sh
+++ init.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 pg_ctlcluster 12 main start
-sudo -u postgres psql -c "CREATE ROLE redmine LOGIN ENCRYPTED PASSWORD 'super_strong_passwd_123' NOINHERIT VALID UNTIL 'infinity'"
-sudo -u postgres psql -c "CREATE DATABASE redmine WITH ENCODING='UTF8' OWNER=redmine"
-source /usr/local/rvm/scripts/rvm
-rvm use 2.6.5
-RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
 ruby bin/rails server -b 0.0.0.0 -e production
--- ruby.sh
+++ ruby.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable --ruby
+source /usr/local/rvm/scripts/rvm
+rvm install 2.6.5
+rvm use 2.6.5
 gem install bundler 
 bundle config set without 'development test rmagick'
 cd /opt/redmine-4.1 && bundle install
 bundle exec rake generate_secret_token
 groupadd postgresusers && usermod -aG postgresusers,sudo postgres && chgrp postgresusers /opt/
-
+sudo -u postgres psql -c "CREATE ROLE redmine LOGIN ENCRYPTED PASSWORD 'super_strong_passwd_123' NOINHERIT VALID UNTIL 'infinity'"
+sudo -u postgres psql -c "CREATE DATABASE redmine WITH ENCODING='UTF8' OWNER=redmine"
+RAILS_ENV=production bundle exec rake db:migrate
+RAILS_ENV=production REDMINE_LANG=ru bundle exec rake redmine:load_default_data
